e93e6577b759aeedc4cb34666062ccec2922c6b6
Cristian Echeverria <cristian.echeverria@etraveligroup.com>
2025-08-13 22:14:19 +0200

    Finally fixed nvim-treesitter, just needed to open neovim :h nvim-treesittee for instructions

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -43,3 +43,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end,
 })
+
+-- Activate nvim-treesitter
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'go', 'javascript', 'typescrypt', 'python', 'rust', 'lua', 'html' },
+  callback = function()
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start()
+    -- folds, provided by Neovim
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,3 @@
-vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" } })
 
-require('nvim-treesitter').setup({ ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }, auto_install = true, sync_install = false, highlight = { enable = true, }, })
+require 'nvim-treesitter'.install { 'rust', 'javascript', 'typescript', 'python', 'go', 'rust', 'lua', 'html' }
