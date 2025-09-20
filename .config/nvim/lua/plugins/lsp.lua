c63830dd685d2aade0b77efb645c36ee654ad6f2
Cristian Echeverria <cristian.echeverria@etraveligroup.com>
2025-09-20 06:54:07 +0200

    Added rust LSP and more treesitter languages

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,34 +1,48 @@
 vim.pack.add({
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/mason-org/mason-lspconfig.nvim" }
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "gopls",
-    "pyright",
-    "eslint",
-    "vtsls",
-    "lua_ls",
-  },
+	ensure_installed = {
+		"gopls",
+		"pyright",
+		"eslint",
+		"vtsls",
+		"lua_ls",
+		"rust_analyzer",
+	},
 })
 
 -- Disable vim undefined warnings
 vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
+
+vim.lsp.config("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+			checkOnSave = {
+				command = "clippy",
+			},
+		},
+	},
 })
 
 -- Diagnostics configuration
 vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true
-  }
+	virtual_lines = {
+		current_line = true,
+	},
 })
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,17 @@
 vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" } })
 
-require 'nvim-treesitter'.install { 'rust', 'javascript', 'typescript', 'python', 'go', 'rust', 'lua', 'html' }
+require("nvim-treesitter").install({
+	"rust",
+	"javascript",
+	"typescript",
+	"python",
+	"go",
+	"rust",
+	"lua",
+	"html",
+	"css",
+	"json",
+	"yaml",
+	"toml",
+	"markdown",
+})
